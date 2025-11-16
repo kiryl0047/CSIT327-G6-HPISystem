67f757bbbadd3e14ca3d240280589b595fc6684c
Andrei Mesiah M. Canoy <kiryl0047@gmail.com>
2025-11-16 16:08:26 +0800

    Change temp_password to string format

--- hpis/main/views.py
+++ hpis/main/views.py
@@ -214,7 +214,7 @@ def create_staff(request):
             temp_password = get_random_string(12, alphabet)
             # Create user with temporary password
             # temp_password = User.objects.make_random_password(12)
-            temp_password = 123456
+            temp_password = str(123456)
             user = User.objects.create_user(
                 username=username,
                 email=email,
